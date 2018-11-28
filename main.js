4b7962bc736c42a1aa333c17b7ff3453111cb9b1
Chamunorwa Mutezva <ckmutezva@gmail.com>
2018-11-28 04:56:20 +0200

    feat: Fixed scrolls on modal page

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let display = document.querySelector(".fa-bars");
 let modal = document.getElementById("modal");
+
 display.addEventListener("click", function () {
     modal.classList.toggle("show");
+    hideScrolls();
     console.log("modal open");
 })
 modal.firstElementChild.addEventListener("click", function () {
     modal.classList.toggle("show");
+    hideScrolls();
     console.log("modal closed");
 })
+
+function hideScrolls() {
+    if (document.body.style.overflow === "hidden") {
+        document.body.style.overflow = "visible";
+    } else {
+        document.body.style.overflow = "hidden";
+    }
+}
